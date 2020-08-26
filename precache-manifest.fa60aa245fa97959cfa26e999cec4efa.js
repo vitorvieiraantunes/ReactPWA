eb0955eb8139be10524f8f0b63573dba3f95bb8f
Vitor <vitorvieira.antunes@gmail.com>
2020-08-26 19:50:33 -0400

    Updates

---
+++ precache-manifest.fa60aa245fa97959cfa26e999cec4efa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d9c8c99a68731a64c2e51d5aabf568",
+    "url": "/%7BGithub-repo-name%7D/index.html"
+  },
+  {
+    "revision": "151d3794fa07af8dd5a2",
+    "url": "/%7BGithub-repo-name%7D/static/css/main.e7af5381.chunk.css"
+  },
+  {
+    "revision": "c7ce8a637e53de674c1f",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.5b25fff6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.5b25fff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "151d3794fa07af8dd5a2",
+    "url": "/%7BGithub-repo-name%7D/static/js/main.f8553fac.chunk.js"
+  },
+  {
+    "revision": "2031e613dfaee30e6e2c",
+    "url": "/%7BGithub-repo-name%7D/static/js/runtime-main.a19b65ee.js"
+  }
+]);
